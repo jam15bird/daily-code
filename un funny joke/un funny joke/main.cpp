a907b23ce27ac8a47ccd59124e01913fd0c4891d
Game110AM <Game110AM@605-110-STU12>
2017-01-31 07:47:43 -0700

    HAHA

---
+++ un funny joke/un funny joke/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout<< "what's the difference between a dirty bus stop and a lobster with breast implants?" << endl;
+	cout << "one's a crusty bus station and the other is a busty crustation" << endl;
+}
